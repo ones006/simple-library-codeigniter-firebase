c0af00a2b7dc854a3f981355c248ea0cb619748d
Iwan Sunarya <ones006@gmail.com>
2019-03-13 11:15:50 +0700

    Update firebase.php

--- config/firebase.php
+++ config/firebase.php
@@ -1,4 +1,4 @@
 <?php
-$config['app_path'] = 'https://timeline-aa73f.firebaseio.com';
-$config['app_key'] = 'WXBbmwvkS6qw7p17x1aXjWilPm4Q2Nz7lHUxJ5V2';
+$config['app_path'] = 'https://YOURPROJECT.firebaseio.com';
+$config['app_key'] = 'ADMINSDKKEY';
 $config['timeout'] = 10;
